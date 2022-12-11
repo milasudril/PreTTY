7494b1f5b6c809a82b6d99cfe37effc877976627
Torbjörn Rathsman <milasudril@gmail.com>
2022-12-11 22:31:15 +0100

    Compile and run code

--- server/main.py
+++ server/main.py
@@ -9,9 +9,72 @@ import sys
 from string import Template
 import cgi
 import urllib.parse
+import tempfile
+import subprocess
+import html
 
-PORT = 8020
+PORT = 8001
 server_dir = Path(__file__).parents[1] / 'client'
+
+def escape_html(str):
+	return html.escape(str)
+
+def fixed_width(str):
+	return '<pre>\n' + escape_html(str) + '</pre>\n'
+
+def compile_and_execute(source_code, output_stream):
+	template = Template('''<!DOCTYPE html>
+<html>
+<head>
+<title>Devbench output</title>
+<style>
+*{font-family:"Andika"}
+code,pre{font-family:"Cascadia code"; font-size:14px}
+</style>
+</head>
+<body>
+<h1>Devbench output</h1>
+<section>
+<h1>Compiler output</h1>
+$compiler_output
+</section>
+<section>
+<h1>Application output</h1>
+$application_output
+</section>
+</body>
+''')
+	with tempfile.TemporaryDirectory() as temp_dir:
+		src_file_name = temp_dir + '/src.cpp'
+		with open(src_file_name, 'wb') as src_file:
+			src_file.write(source_code.encode('utf-8'))
+
+		exec_name = temp_dir + '/src.out'
+		compiler_result = subprocess.run(['g++',
+			'-std=c++20',
+			'-O3',
+			'-ffast-math',
+			'-Wall',
+			'-Wextra',
+			'-Wconversion',
+			'-Werror',
+			src_file_name,
+			'-o',
+			exec_name],
+			capture_output=True)
+		application_output = 'Compilation failed'
+		compiler_output = 'Compilation succeeded'
+
+		if compiler_result.returncode == 0:
+			exec_result = subprocess.run([exec_name], capture_output=True)
+			application_output = exec_result.stdout.decode('utf-8')
+		else:
+			compiler_output = fixed_width(compiler_result.stderr.decode('utf-8'))
+
+		res = template.substitute(compiler_output = compiler_output,
+			application_output = application_output)
+
+		output_stream.write(res.encode('utf-8'))
 
 class HttpReqHandler(http.server.SimpleHTTPRequestHandler):
 	def do_GET(self):
@@ -28,15 +91,15 @@ class HttpReqHandler(http.server.SimpleHTTPRequestHandler):
 					self.wfile.write(content_file.read())
 			except:
 				super(HttpReqHandler, self).do_GET()
+
 	def do_POST(self):
-		self.wfile.write(('%s 200\r\nContent-Type: text/plain\r\n\r\n' % self.request_version).encode('utf-8'))
 		ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
 		pdict['boundary'] = bytes(pdict['boundary'], 'utf-8')
 		pdict['CONTENT-LENGTH'] = int(self.headers.get('Content-length'))
 		parsed_data = cgi.parse_multipart(self.rfile, pdict)
-		self.wfile.write(parsed_data['source'][0].encode('utf-8'))
-
-
+		self.wfile.write(('%s 200\r\nContent-Type: text/html\r\n\r\n' %
+					self.request_version).encode('utf-8'))
+		compile_and_execute(parsed_data['source'][0], self.wfile)
 
 handler = HttpReqHandler
 
