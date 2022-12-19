e6e2695d9d388ef157ba2130b018a42f6cdddad7
Torbjörn Rathsman <milasudril@gmail.com>
2022-12-19 22:09:03 +0100

    Implement auto-range

--- lib/cxx/pretty/plot.hpp
+++ lib/cxx/pretty/plot.hpp
@@ -12,6 +12,13 @@ namespace pretty
 		X min;
 		X max;
 	};
+/*
+	template<class X>
+	plot_axis_range(X, X) -> plot_axis_range<X>;
+
+	template<class X>
+	plot_axis_range(X const&, X const&) -> plot_axis_range<X>;
+*/
 
 	struct plot_line_style
 	{
@@ -46,7 +53,7 @@ namespace pretty
 	};
 
 	template<class T>
-	concept plot_data_2d = std::ranges::input_range<T> && plot_point_2d<std::ranges::range_value_t<T>>;
+	concept plot_data_2d = std::ranges::forward_range<T> && plot_point_2d<std::ranges::range_value_t<T>>;
 
 	template<plot_data_2d T>
 	struct make_plot_params_2d
@@ -63,17 +70,42 @@ namespace pretty
 	template<plot_data_2d T>
 	using plot_params_2d_t = make_plot_params_2d<T>::type;
 
+	template<size_t Element, plot_data_2d PlotData>
+	auto compute_range(PlotData const& data)
+	{
+		auto minmax = std::ranges::minmax_element(data, [](auto const& a, auto const& b) {
+			return get<Element>(a) < get<Element>(b);
+		});
+
+		return plot_axis_range{get<Element>(*minmax.min), get<Element>(*minmax.max)};
+	}
+
 	template<plot_data_2d PlotData>
 	void plot(PlotData const& plot_data,
-		plot_params_2d_t<PlotData> const& = plot_params_2d_t<PlotData>{})
+		plot_params_2d_t<PlotData> const& plot_params = plot_params_2d_t<PlotData>{})
 	{
-		atomic_write([](auto const& range){
-			puts("<div style=\"width:80%; aspect-ratio:16/10; border:1px solid; margin-left:auto; margin-right:auto\">");
-			puts("<svg viewbox=\"-1024 -1024 2048 2048\" width=\"100%\" height=\"100%\">");
+		auto const x_range = plot_params.x_range.value_or(compute_range<0>(plot_data));
+		auto const y_range = plot_params.y_range.value_or(compute_range<1>(plot_data));
+
+		atomic_write([](auto const& range, auto x_range, auto y_range){
+			auto const factor = 1024.0f;
+			auto const width = x_range.max - x_range.min;
+			auto const height = y_range.max - y_range.min;
+
+			puts("<div style=\"width:80%; max-height:30vh; aspect-ratio:16/10; border:1px solid; margin-left:auto; margin-right:auto\">");
+			write_raw("<svg viewbox=\"");
+			write_raw(std::data(to_char_buffer(factor*x_range.min)));
+			putchar(' ');
+			write_raw(std::data(to_char_buffer(factor*y_range.min)));
+			putchar(' ');
+			write_raw(std::data(to_char_buffer(factor*width)));
+			putchar(' ');
+			write_raw(std::data(to_char_buffer(factor*height)));
+			write_raw("\" width=\"100%\" height=\"100%\">");
 			puts("<polyline stroke-width=\"4\" stroke=\"blue\" points=\"");
-			std::ranges::for_each(range, [](auto const& item) {
-				auto const x =  1024.0f*get<0>(item);
-				auto const y = -1024.0f*get<1>(item);
+			std::ranges::for_each(range, [factor, height](auto const& item) {
+				auto const x = factor*get<0>(item);
+				auto const y = factor*get<1>(item);
 				write_raw(std::data(to_char_buffer(x)));
 				putchar(',');
 				write_raw(std::data(to_char_buffer(y)));
@@ -82,7 +114,7 @@ namespace pretty
 			puts("\" fill=\"none\"/>");
 			puts("</svg>");
 			puts("</div>");
-		}, plot_data);
+		}, plot_data, x_range, y_range);
 	}
 }
 
--- samples/cxx/plot.cpp
+++ samples/cxx/plot.cpp
@@ -7,12 +7,13 @@ using my_vector = geosimd::euclidian_space<float, 3>::displacement;
 
 int main()
 {
-	std::array<my_point, 12> vals{};
-	for(size_t k = 0; k != 12; ++k)
+	constexpr auto N = 128;
+	std::array<my_point, 12*N + 1> vals{};
+	for(int32_t k = 0; k != 12*N + 1; ++k)
 	{
-		auto const theta = geosimd::rotation_angle{geosimd::turns{static_cast<double>(k)/12}};
+		auto const theta = geosimd::rotation_angle{geosimd::rad{static_cast<double>(k)/N}};
 		auto cs = cossin(theta);
-		vals[k] = my_point{} + 1.0f*my_vector{cs.cos, cs.sin, 0.0f};
+		vals[k] = my_point{} + my_vector{static_cast<float>(k)/N, cs.sin, 0.0f};
 	}
 
 	pretty::plot(vals);
