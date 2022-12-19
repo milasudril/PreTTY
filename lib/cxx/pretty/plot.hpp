4ad9ea5543f9c6d6cdf0c6a585f4238757e4d0f5
Torbjörn Rathsman <milasudril@gmail.com>
2022-12-19 18:38:15 +0100

    Do not use printf

--- lib/cxx/pretty/plot.hpp
+++ lib/cxx/pretty/plot.hpp
@@ -13,7 +13,12 @@ namespace pretty
 			puts("<svg viewbox=\"-1024 -1024 2048 2048\" width=\"100%\" height=\"100%\">");
 			puts("<polyline stroke-width=\"4\" stroke=\"blue\" points=\"");
 			std::ranges::for_each(range, [](auto const& item) {
-				printf("%.8e,%.8e ", 1024.0f*get<0>(item), -1024.0f*get<1>(item));
+				auto const x =  1024.0f*get<0>(item);
+				auto const y = -1024.0f*get<1>(item);
+				write_raw(std::data(to_char_buffer(x)));
+				putchar(',');
+				write_raw(std::data(to_char_buffer(y)));
+				putchar(' ');
 			});
 			puts("\" fill=\"none\"/>");
 			puts("</svg>");
